075bf4dec684b6548fcb7da0c3eea264332ab8c4
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2021-10-18 23:55:06 +0530

    using useState instead of useRef

--- components/Header.js
+++ components/Header.js
@@ -1,24 +1,22 @@
 import { XIcon, MicrophoneIcon, SearchIcon } from "@heroicons/react/solid";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { useRef } from "react";
+import { useState } from "react";
 import Avatar from "./Avatar";
 import HeaderOptions from "./HeaderOptions";
 
 export default function Header() {
   const router = useRouter();
-  const searchInputRef = useRef(null);
+  const [searchInput, setSearchInput] = useState("");
 
   function search(e) {
     e.preventDefault();
-    const term = searchInputRef.current.value;
-    if (!term) return;
-
-    router.push(`/search?term=${term}`);
+    if (!searchInput) return;
+    router.push(`/search?term=${searchInput}`);
   }
 
   return (
-    <header className="sticky top-0 bg-white font-sans">
+    <header className="sticky top-0 bg-white dark:bg-primary-dark dark:text-white font-sans">
       <div className="flex w-full p-6 items-center justify-between flex-col sm:flex-row">
         <Image
           src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c51f.png"
@@ -28,18 +26,23 @@ export default function Header() {
           onClick={() => router.push("/")}
         />
 
-        <form className="flex w-full px-6 py-3 sm:ml-10 sm:mr-5 border-gray-200 rounded-full shadow-md max-w-3xl flex-grow items-center">
+        <form className="flex w-full px-6 py-3 mt-5 sm:mt-0 sm:ml-10 sm:mr-5 border-gray-200 rounded-full shadow-md max-w-3xl flex-grow items-center dark:bg-secondary-dark">
           <input
             type="text"
-            ref={searchInputRef}
-            className="flex-grow w-full focus:outline-none"
+            // ref={searchInputRef}
+            value={searchInput}
+            onChange={(e) => setSearchInput(e.target.value)}
+            placeholder="Search..."
+            className="flex-grow w-full focus:outline-none dark:bg-secondary-dark"
             defaultValue={router.query.term}
           />
 
-          <XIcon
-            className="h-7 sm:mr-3 text-gray-500 cursor-pointer transition duration-100 transform hover:scale-110"
-            onClick={() => (searchInputRef.current.value = "")}
-          />
+          {searchInput && (
+            <XIcon
+              className="h-6 sm:mr-3 text-gray-500 cursor-pointer transition duration-100 transform hover:scale-110"
+              onClick={() => setSearchInput("")}
+            />
+          )}
 
           <MicrophoneIcon className="mr-3 h-6 hidden sm:inline-flex text-blue-500 border-l-2 pl-4 border-gray-400 cursor-pointer" />
 
