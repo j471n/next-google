d1dc14b27866360885f6a1d3aa1370aa9e237f78
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2021-10-19 12:51:12 +0530

    using PWA

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
-module.exports = {
+const withPWA = require("next-pwa");
+module.exports = withPWA({
   reactStrictMode: true,
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  },
   images: {
     domains: ["assets.stickpng.com", "i.imgur.com"],
   },
-};
+});
