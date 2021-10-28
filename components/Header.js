7d7426c94d7dbb09d1cdba2ffbc67a7bc3cd1d15
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2021-10-28 22:06:16 +0530

    Fixing space problem in header

--- components/Header.js
+++ components/Header.js
@@ -17,42 +17,44 @@ export default function Header() {
 
   return (
     <header className="sticky top-0 bg-white dark:bg-primary-dark dark:text-white font-sans">
-      <div className="flex w-full p-6 items-center justify-between flex-col sm:flex-row">
-        <Image
-          src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c51f.png"
-          height={40}
-          width={120}
-          className="cursor-pointer"
-          onClick={() => router.push("/")}
-          alt="Google"
-        />
-
-        <form className="flex w-full px-6 py-3 mt-5 sm:mt-0 sm:ml-10 sm:mr-5 border-gray-200 rounded-full shadow-md max-w-3xl flex-grow items-center dark:bg-secondary-dark">
-          <input
-            type="text"
-            // ref={searchInputRef}
-            value={searchInput}
-            onChange={(e) => setSearchInput(e.target.value)}
-            placeholder="Search..."
-            className="flex-grow w-full focus:outline-none dark:bg-secondary-dark"
+      <div className="flex w-full p-6 items-center justify-between">
+        <div className="flex items-center flex-col sm:flex-row w-full ">
+          <Image
+            src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c51f.png"
+            height={40}
+            width={120}
+            className="cursor-pointer"
+            onClick={() => router.push("/")}
+            alt="Google"
           />
 
-          {searchInput && (
-            <XIcon
-              className="h-6 sm:mr-3 text-gray-500 cursor-pointer transition duration-100 transform hover:scale-110"
-              onClick={() => setSearchInput("")}
+          <form className="flex w-full px-6 py-3 mt-5 sm:mt-0 sm:ml-10 sm:mr-5 border-gray-200 rounded-full shadow-md max-w-3xl flex-grow items-center dark:bg-secondary-dark">
+            <input
+              type="text"
+              // ref={searchInputRef}
+              value={searchInput}
+              onChange={(e) => setSearchInput(e.target.value)}
+              placeholder="Search..."
+              className="flex-grow w-full focus:outline-none dark:bg-secondary-dark"
             />
-          )}
 
-          <MicrophoneIcon className="mr-3 h-6 hidden sm:inline-flex text-blue-500 border-l-2 pl-4 border-gray-400 cursor-pointer" />
+            {searchInput && (
+              <XIcon
+                className="h-6 sm:mr-3 text-gray-500 cursor-pointer transition duration-100 transform hover:scale-110"
+                onClick={() => setSearchInput("")}
+              />
+            )}
 
-          <button type="submit" onClick={search}>
-            <SearchIcon className="h-6 text-blue-500 pl-2 cursor-pointer" />
-          </button>
-        </form>
+            <MicrophoneIcon className="mr-3 h-6 hidden sm:inline-flex text-blue-500 border-l-2 pl-4 border-gray-400 cursor-pointer" />
+
+            <button type="submit" onClick={search}>
+              <SearchIcon className="h-6 text-blue-500 pl-2 cursor-pointer" />
+            </button>
+          </form>
+        </div>
 
         <div className="hidden sm:inline-flex">
-          <Avatar className="ml-auto" />
+          <Avatar className="ml-auto" url="/img/profile.jpg" />
         </div>
       </div>
       <HeaderOptions />
