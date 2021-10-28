5558fbf7d98c9c178be63d57e562636c5255d731
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2021-10-28 22:06:03 +0530

    Update Avatar.js

--- components/Avatar.js
+++ components/Avatar.js
@@ -3,7 +3,7 @@ export default function Avatar({ url, className }) {
   const defaultURL = "https://i.imgur.com/kGTbP8v.png";
   return (
     <Image
-      className={`h-10 w-10 rounded-full cursor-pointer transition duration-150 transform hover:scale-110 ${className}`}
+      className={`h-10 w-10 rounded-full cursor-pointer duration-150 hover:animate-pulse ${className}`}
       src={url ? url : defaultURL}
       alt="profile pic"
       width={40}
